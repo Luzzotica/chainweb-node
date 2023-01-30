39be4867835aaca8a9e6bb87ad1ce642ba655241
Stuart Popejoy <sirlensalot@users.noreply.github.com>
2023-01-30 17:16:12 -0500

    cleanup

--- test/Chainweb/Test/Pact/Checkpointer.hs
+++ test/Chainweb/Test/Pact/Checkpointer.hs
@@ -107,6 +107,7 @@ testModuleName = withTempSQLiteResource $
 
 -- -------------------------------------------------------------------------- --
 -- Key Set Test
+
 testKeyset :: TestTree
 testKeyset = withResource initializeSQLite freeSQLiteResource (runSQLite keysetTest)
 
